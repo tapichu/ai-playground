c5d83f2286b1306348d3119b35e7c8d15100a5ee
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2011-12-14 14:46:05 -0600

    Rotation Cipher decoding using gzip

---
+++ src/rotation_cipher_gzip.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "Eduardo Lopez Biagi"
+__license__ = "BSD-new"
+
+"""
+Use gzip to 'break' a rotation cipher.
+
+Concatenate each possible encoding (rotation) with a text in english.
+Run this through gzip and count the number of characters in the output.
+The smallest one should be the one in english.
+
+Requirements:
+    * Python 3.x
+    * Basic UNIX commands: gzip, cat, echo, wc, sort, head, cut
+"""
+
+from rotation_cipher_plm import TEXT, RotationCipher
+import logging
+import re
+import subprocess
+
+# Logging level
+logging.basicConfig(level=logging.INFO)
+
+
+def run_command(command):
+    """Spawn a new process to execute a command on the shell"""
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout_value, stderr_value = proc.communicate()
+
+    if proc.returncode > 1:
+        logging.error(stderr_value)
+        return -1
+    else:
+        logging.debug(stdout_value)
+        return int(stdout_value)
+
+def most_probable(phrases):
+    """
+    Build a command that runs each phrase through gzip. Select the phrase that produces the
+    smallest gzip output (no. of characters).
+    """
+    idx = 0
+    command = "("
+    for phrase in phrases:
+        command += 'echo `echo "%s $(cat text_en.txt)" | gzip | wc -c` %i; ' % (phrase, idx)
+        idx += 1
+    command += ") | sort -n | head -1 | cut -d ' ' -f 2"
+    logging.debug("Command: %s", command)
+
+    best_idx = run_command(command)
+    return phrases[best_idx] if best_idx >= 0 else "An error occurred"
+
+def main():
+    # Text cleanup: remove punctuation characters, etc.
+    text = re.sub("[^a-z ]", "", TEXT.lower())
+
+    rotation_cipher = RotationCipher()
+    all_phrases = [rotation_cipher.cipher(text, x) for x in range(0, 26)]
+
+    # Figure out the probability of each possible shift
+    phrase = most_probable(all_phrases)
+    print("Most probable phrase: %s" % phrase)
+
+if __name__ == "__main__":
+    main()
---
+++ src/tests/rotation_cipher_gzip_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "Eduardo Lopez Biagi"
+__license__ = "BSD-new"
+
+from .. import rotation_cipher_gzip as rcgzip
+import unittest
+
+class TestRotationCipherGzip(unittest.TestCase):
+
+    def setUp(self):
+        self.rc = rcgzip.RotationCipher()
+        self.phrase = "Your highness, when I said that you are like a stream of bat's \
+piss, I only mean that you shine out like a shaft of gold when all around it is dark"
+        self.encoded_phrase = self.rc.cipher(self.phrase, 5)
+
+    def test_run_command(self):
+        self.assertEqual(rcgzip.run_command('echo "10"'), 10)
+
+    def test_run_command_error(self):
+        self.assertEqual(rcgzip.run_command('not_found'), -1)
+
+    def test_most_probable(self):
+        phrases = [self.rc.cipher(self.encoded_phrase, x) for x in range(0, 26)]
+        phrase = rcgzip.most_probable(phrases)
+        self.assertEqual(phrase, self.phrase.lower())
+
+if __name__ == '__main__':
+    unittest.main()
